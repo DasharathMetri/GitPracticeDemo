76ded50cc2cf4123f07f2c473abc460b002c150b
dasharath <dasharathmetri1996@gmail.com>
2023-02-12 23:43:21 +0530

    Updated base class

--- src/main/java/com/qa/base/BaseClass.java
+++ src/main/java/com/qa/base/BaseClass.java
@@ -3,6 +3,7 @@ package com.qa.base;
 public class BaseClass {
 	public void openBrowser() {
 		System.out.println("open browser");
+		System.out.println("making some chnages and pushing code to repo");
 	}
 
 }
