53bfe1f64031d82ad16375c360070599438bf7d5
dasharath <dasharathmetri1996@gmail.com>
2023-02-14 00:23:34 +0530

    changes made by person x

--- src/main/java/com/qa/base/BaseClass.java
+++ src/main/java/com/qa/base/BaseClass.java
@@ -1,9 +1,14 @@
 package com.qa.base;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class BaseClass {
 	public void openBrowser() {
 		System.out.println("open browser");
 		System.out.println("making some chnages and pushing code to repo");
+	}
+	public void personx(){
+		System.out.println("person x changes");
 	}
 
 }
